bfd9f746bcaa85fdb84eb9c4e4433f85a306065b
Igor Kh <igor150195@gmail.com>
2025-08-19 23:05:09 +0500

    создал компонент Note2.jsx и вывовдил его в App.jsx. повторяю уроки

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from "react";
 import NoteList from "./components/NoteList";
 import NoteForm from "./components/NoteForm";
+import Note2 from "./components/Note2";
 
 export default function App() {
   const [notes, setNotes] = useState([
     { id: 1, text: "Первая заметка", description: "подчеркнул" },
     { id: 2, text: "Вторая заметка" },
     { id: 3, text: "3 заметка" },
+  ]);
+
+  const [myNotes, updateNotes] = useState([
+    { id: Math.random() * 10, text: "11 заметка" },
+    { id: Math.random() * 10, text: "22 заметка" },
+    { id: Math.random() * 10, text: "333 заметка" },
+    { id: Math.random() * 10, text: "443 заметка" },
   ]);
 
   const handleDelete = (deleteId) => {
@@ -26,11 +34,31 @@ export default function App() {
     setNotes([...notes, newNote]);
   };
 
+  const myFunc = (id) => {
+    return myNotes.filter((myNote) => myNote.id !== id);
+  };
+
+  const deleteNote = (id) => {
+    updateNotes(myFunc(id));
+  };
+
   return (
     <div>
       <h1>Notes App</h1>
       <NoteList notes={notes} onDelete={handleDelete} />
       <NoteForm onAdd={handleOnAdd} />
+      <br />
+      <br />
+      <hr />
+
+      {myNotes.map((note) => (
+        <Note2
+          key={note.id}
+          id={note.id}
+          text={note.text}
+          onDelete={deleteNote}
+        />
+      ))}
     </div>
   );
 }
